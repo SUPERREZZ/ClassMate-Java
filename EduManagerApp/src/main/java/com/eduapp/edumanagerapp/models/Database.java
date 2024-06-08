0c34e8c340e973d9e0a5704c47947454852aa0eb
Reza Ghiyats Fikri <141542911+SUPERREZZ@users.noreply.github.com>
2024-06-08 19:01:38 +0700

    Update Database.java

--- EduManagerApp/src/main/java/com/eduapp/edumanagerapp/models/Database.java
+++ EduManagerApp/src/main/java/com/eduapp/edumanagerapp/models/Database.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 
 public class Database {
-    private static final String test = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres";
-    private static final String USERNAME = "postgres.fkrmgqmyawbzueewvdlq";
-    private static final String PASSWORD = "vNkBiSRNW0P0B8tO";
+    private static final String test = "";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(test, USERNAME, PASSWORD);
